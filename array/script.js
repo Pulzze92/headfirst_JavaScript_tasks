29e41e03f1c0b205a55d4ab661de20b65c5d3aa1
Pulzze92 <89998310392@mail.ru>
2021-02-25 13:03:08 +0300

    added functions to array chapter

--- array/script.js
+++ array/script.js
@@ -1,25 +1,34 @@
 let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
-let highScore = 0;
-let output;
 let out = document.querySelector('.out');
 
-for (let i = 0; i < scores.length; i++) {
-	output = 'Bubble solution #' + i + ' score: ' + scores[i] + '<br>';
-	out.innerHTML += output;
-	if (scores[i] > highScore) {
-		highScore = scores[i];
+function printAndGetHighScore (scores) {
+	let highScore = 0;
+	let output;
+	for (let i = 0; i < scores.length; i++) {
+		output = 'Bubble solution #' + i + ' score: ' + scores[i] + '<br>';
+		out.innerHTML += output;
+		if (scores[i] > highScore) {
+			highScore = scores[i];
+		}
 	}
+	return highScore;	
 }
+
+function getBestResults(scores, highScore) {
+	let bestSolutions = [];
+	for (let i = 0; i < scores.length; i++) {
+		if (scores[i] == highScore) {
+			bestSolutions.push(i);
+		}
+	}
+	return bestSolutions;
+}
+
+let highScore = printAndGetHighScore(scores);
 
 document.querySelector('.out_highscore').innerHTML += 'Bubbles tests: ' + scores.length + '<br>';
 document.querySelector('.out_highscore').innerHTML += 'Highest bubble score: ' + highScore;
 
-let bestSolutions = [];
+let bestSolutions = getBestResults(scores, highScore);
 
-for (let i = 0; i < scores.length; i++) {
-	if (scores[i] == highScore) {
-		bestSolutions.push(i);
-	}
-}
-
-document.querySelector('.out_array').innerHTML += 'Solutions with the highest score: ' + bestSolutions;
+document.querySelector('.out_array').innerHTML = 'Solutions with the highest score: ' + bestSolutions;
